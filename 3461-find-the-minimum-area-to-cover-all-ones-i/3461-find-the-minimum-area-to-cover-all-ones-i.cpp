a8be9afe59a62bc53b626c6c92fb9690e6824e13
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-06-25 07:07:49 +0530

    Time: 265 ms (58.18%), Space: 132.2 MB (81.45%) - LeetHub

--- 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3461-find-the-minimum-area-to-cover-all-ones-i/3461-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -1,28 +1,28 @@
 class Solution {
+    
 public:
     int minimumArea(vector<vector<int>>& grid) {
-        //Try to store the i and j indexs for grid[i][j] = 1 in these two vector 
-       vector<int>horizontal_index;
-       vector<int>vertical_index;
+        
+        int minrow=INT_MAX,maxrow=0,mincol=INT_MAX,maxcol=0;
+         
         
         
-        for(int i = 0 ; i< grid.size();i++){
-            
-            for(int j= 0 ; j < grid[0].size() ;j++){
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+                if(grid[i][j]==1){
+                    minrow=min(minrow,i);
+                    maxrow=max(maxrow,i);
+                    mincol=min(mincol,j);
+                    maxcol=max(maxcol,j);
+
+                }
                 
-               if(grid[i][j]==1){
-                   horizontal_index.push_back(j);
-                   vertical_index.push_back(i);
-               }
             }
         }
-        
-        // Now we can sort the vector and find the min and max i and j for grid[i][j] = 1
-        sort(horizontal_index.begin(),horizontal_index.end());
-        sort(vertical_index.begin(),vertical_index.end());
-        
-        //Finally caluculate the area of rec.
-        return (horizontal_index[horizontal_index.size()-1]-horizontal_index[0]+1)*(vertical_index[vertical_index.size()-1]-vertical_index[0]+1);
-        
+
+        int width= maxcol-mincol+1;
+        int height= maxrow-minrow+1;
+        cout<<width*height;
+        return width*height;
     }
 };
