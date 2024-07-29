341d6e596fc2ec1af65f498cffc202fa8515b515
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-07-29 06:34:53 +0530

    Time: 1950 ms (20.65%), Space: 11.7 MB (40.04%) - LeetHub

---
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        
+        int res=0;
+
+        for(int i=0;i<rating.size()-2;i++){
+
+            for(int j=i+1;j<rating.size()-1;j++){
+
+                if(rating[i]<rating[j]){
+
+                    for(int k=j+1;k<rating.size();k++){
+                        
+                        if(rating[j]<rating[k]) res++;
+                    }
+                }
+
+                else if(rating[i]>rating[j]){
+
+                    for(int k=j+1;k<rating.size();k++){
+                        
+                        if(rating[j]>rating[k]) res++;
+                    }
+                }
+                
+            }
+        }
+
+        return res;
+    }
+};
