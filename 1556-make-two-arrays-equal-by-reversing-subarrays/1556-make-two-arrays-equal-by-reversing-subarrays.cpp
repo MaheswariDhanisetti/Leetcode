fc7042f356540f91ff6543ce71a5dcc0950ae453
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2024-08-05 23:06:11 +0530

    Time: 11 ms (42.02%), Space: 17.5 MB (64.65%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool canBeEqual(std::vector<int>& target, std::vector<int>& arr) {
-        std::sort(target.begin(), target.end());
-        std::sort(arr.begin(), arr.end());
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
         for (size_t i = 0; i < arr.size(); ++i) {
             if (arr[i] != target[i]) {
                 return false;
