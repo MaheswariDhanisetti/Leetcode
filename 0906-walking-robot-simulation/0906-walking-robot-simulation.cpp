729c03c988cef6df18a94329a51456e5931d8a4b
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2024-09-05 15:40:15 +0530

    Time: 66 ms (79.89%), Space: 38.4 MB (68.75%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -1,39 +1,104 @@
 class Solution {
 public:
-    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        int x = 0, y = 0, direction = 0, maxDist = 0;
-        set<pair<int, int>> obstacleSet;
 
-        for(const auto& ob:obstacles){
-            obstacleSet.insert({ob[0], ob[1]});
+    void changeDirRight(char &currDir){
+        if(currDir=='N'){
+            currDir='E';
+        }
+        else if(currDir=='E'){
+            currDir='S';
+        }
+        else if(currDir=='W'){
+            currDir='N';
+        }
+        else{
+            currDir='W';
+        }
+    }
+
+    void changeDirLeft(char &currDir){
+        if(currDir=='N'){
+            currDir='W';
+        }
+        else if(currDir=='E'){
+            currDir='N';
+        }
+        else if(currDir=='W'){
+            currDir='S';
+        }
+        else{
+            currDir='E';
+        }
+    }
+
+    void makeMove(char &currDir,int &x,int &y,int dist,set<pair<int,int>>&st){
+
+        
+        for(int i=1;i<=dist;i++){
+
+            if(currDir=='E'){
+                if(st.find({x+1,y})==st.end()){
+                    x++;
+                }
+                else break;
+            }
+            else if(currDir=='W'){
+                if(st.find({x-1,y})==st.end()){
+                    x--;
+                }
+                else break;
+            }
+            else if(currDir=='N'){
+                if(st.find({x,y+1})==st.end()){
+                    y++;
+                }
+                else break;
+            }
+            else if(currDir=='S'){
+                if(st.find({x,y-1})==st.end()){
+                    y--;
+                }
+                else break;
+            }
         }
 
-        vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    }
 
-        for(int command:commands){
-            if(command == -1){
-                direction = (direction+1)%4;
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        
+
+        set<pair<int,int>>st;
+
+        for(int i=0;i<obstacles.size();i++){
+            st.insert({obstacles[i][0],obstacles[i][1]});
+        }
+
+        long long ans=0;
+        int x=0,y=0;
+        // if(st.find({0,0})!=st.end()){
+        //     x=0;
+        //     y-=1;
+        // }
+        char currDir='N';
+        for(int i=0;i<commands.size();i++){
+
+            if(commands[i]==-1){
+               changeDirRight(currDir);
             }
-            else if(command == -2){
-                direction = (direction+3)%4;
+            else if(commands[i]==-2){
+                changeDirLeft(currDir);
             }
             else{
-                int steps = command;
-                while(steps){
-                    int newx = x+dir[direction].first;
-                    int newy = y+dir[direction].second;
 
-                    if(obstacleSet.find({newx, newy}) != obstacleSet.end()){
-                        break;
-                    }
+                makeMove(currDir,x,y,commands[i],st);
+                long long curr= (x*x)+(y*y);
+                ans=max(ans, curr );
 
-                    x = newx;
-                    y = newy;
-                    maxDist = max(maxDist, x*x + y*y);
-                    steps--;
-                }
             }
         }
-        return maxDist;
+
+        return ans;
+
+
     }
 };
