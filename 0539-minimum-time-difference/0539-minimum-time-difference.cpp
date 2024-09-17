d34ece058dceca2f1154482e7f88db4b6e0a09e9
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2024-09-17 21:50:18 +0530

    Time: 21 ms (7.95%), Space: 17.4 MB (95.29%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
+
+        //idea or approach :- sort the array of times as basic fact is that the  difference between adjacent times is always minimum
+        //-->findout minutes value of given time 
+        //-->then find the adjacent difference between two minutes 
+        //-->handle an edge case as time is cyclic so consider the differenc of last and firts times in sorted array.
+        //--> when handling edge case you have to add 1440 minutes (24hrs in minutes) to lowest minutes and subtract the maximum miuntes from it.
+        //-->return the minimum values of all adjacent values
+
         sort(timePoints.begin(),timePoints.end());
         int ans=INT_MAX;
         int val;
         timePoints.push_back(timePoints[0]);
+
         for(int i=0;i<timePoints.size()-1;i++){
             string hrs1 = timePoints[i].substr(0,2);
             string mins1 = timePoints[i].substr(3,2);
