0c8759c81bca25363374703a1d21239a7f9672a7
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2024-09-26 14:21:02 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.35%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,14 +3,19 @@ public:
 
     void dfs(string &digits,int idx,unordered_map<char,string>&mp,vector<string>&ans,string curr){
 
+        //if you reached the end of sequence just store your current string or sequence of charcters 
         if(idx>=digits.size()){
             ans.push_back(curr);
         } 
+
+        //traverse in all correspondingncharacters of a digit and call dfs for next digit in sequence 
         for(int i=0;i<mp[digits[idx]].size();i++){
             dfs(digits,idx+1,mp,ans,curr+mp[digits[idx]][i]);
         }
     }
     vector<string> letterCombinations(string digits) {
+
+        //maintaining a hashmap to map each digit to its corresponnding characters
         unordered_map<char,string>mp;
         mp[' ']="";
         mp['2']="abc";
@@ -22,8 +27,13 @@ public:
         mp['8']="tuv";
         mp['9']="wxyz";
 
+
         vector<string>ans;
+
+
         if(digits=="") return ans;
+
+        //call for dfs function starting from first digit in the sequence
         dfs(digits,0,mp,ans,"");
         return ans;
 
