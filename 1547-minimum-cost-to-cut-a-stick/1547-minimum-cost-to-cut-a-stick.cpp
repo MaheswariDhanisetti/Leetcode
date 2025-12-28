b093d7ee393974e2bad0a3795a566fcce8f5e711
Maheswari Dhanisetti <maheswarimahi2005@gmail.com>
2025-12-28 15:57:55 +0530

    Time: 31 ms (53.16%), Space: 11.3 MB (92.94%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
 
+    int dp[101][101];
+
     int solve(vector<int>&cuts ,int i , int j ,int st , int en)
     {
 
         if(i>j) return 0;
+
+        if(dp[i][j]!=-1) return dp[i][j];
 
         int res = INT_MAX;
 
@@ -13,13 +17,15 @@ public:
             res = min(res , (en-st)+solve(cuts , i,k-1 , st,cuts[k])+solve(cuts , k+1 , j , cuts[k] , en) );
         }
 
-        return res;
+        return dp[i][j] = res;
     }
     int minCost(int n, vector<int>& cuts) {
 
         sort(cuts.begin() , cuts.end());
 
         int si = cuts.size();
+
+        memset(dp , -1,sizeof(dp));
 
         return solve(cuts ,0 , si-1, 0,n);
     }
